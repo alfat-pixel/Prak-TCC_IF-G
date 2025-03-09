19689b8b81e67a92657bd3e5a8ab7cc72d717d36
alfat-pixel <alfatrahman11@gmail.com>
2025-03-09 21:53:52 +0700

    Update database.js
    Menambahkan host dan password pada database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize ("tugas-3-tcc-01","root","",{
-    host: "localhost",
+const db = new Sequelize ("tugas-3-tcc-01","root","123220001Belajar",{
+    host: "34.71.73.223",
     dialect : "mysql",
 });
 
