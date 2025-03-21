498b6ff5669022991c54781a59a445820b581f2b
alfatrahman <162115441+alfatrahman@users.noreply.github.com>
2025-03-21 15:32:51 +0700

    UPdate database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize ("tugas-3-tcc-01","root","",{
-    host: "localhost",
+const db = new Sequelize ("notes-01","root","notes123",{
+    host: "107.178.221.91",
     dialect : "mysql",
 });
 
